1215858c127c5adc1ce99d8bc5de2d1672cadac3
JuanFCVal <cevallosjuanfrancisco@gmail.com>
2024-06-27 09:28:52 -0500

    :bug: Fix title size on responsive

--- apps/crawler-front/src/components/NewsCard/NewsCard.tsx
+++ apps/crawler-front/src/components/NewsCard/NewsCard.tsx
@@ -7,7 +7,7 @@ interface INewsCardProps {
 const NewsCard = ({ newsItem }: INewsCardProps) => {
     return (
         <div className="w-full h-full p-4 bg-slate-200/65 rounded-md my-2 transition-transform duration-500 transform hover:scale-105 cursor-pointer">
-            <h2 className="lg:text-xl md:text-lg line-clamp-2 font-semibold min-h-14">{newsItem?.rank}. {newsItem?.title}</h2>
+            <h2 className="lg:text-xl md:text-lg line-clamp-2 font-semibold min-h-fit">{newsItem?.rank}. {newsItem?.title}</h2>
             <div className='flex justify-end text-gray-600 mt-2 items-end'>
                 <div className='hover:text-blue-500 hover:font-medium flex cursor-pointer'>
                     {newsItem?.comments} comments
