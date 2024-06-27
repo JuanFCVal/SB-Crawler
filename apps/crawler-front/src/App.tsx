37eb6cdabbcdd786863f348df70df8d2347dd851
JuanFCVal <cevallosjuanfrancisco@gmail.com>
2024-06-27 09:23:24 -0500

    ✨ Get news in useEffect

--- apps/crawler-front/src/App.tsx
+++ apps/crawler-front/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Layout from "./components/Layout/Layout"
 import { getNews } from "./services/news_service"
 import { INewsItem } from "./types/news_types";
@@ -7,19 +7,19 @@ import NewsList from "./components/NewsList/Newslist";
 function App() {
   const [newsElements, setNewsElements] = useState<INewsItem[]>()
 
-  const handleLoadNews = async () => {
-    const response = await getNews()
-    setNewsElements(response.news)
-  }
+  useEffect(() => {
+    const handleLoadNews = async () => {
+      const response = await getNews()
+      setNewsElements(response.news)
+    }
+    handleLoadNews()
+  }, [])
 
   return (
     <Layout>
       <div className="w-full px-10 md:px-5 sm:px-2">
         <h1 className="text-3xl mb-4">Hacker news</h1>
         <NewsList news={newsElements || []} />
-        <button onClick={handleLoadNews}>
-          Get news
-        </button>
       </div>
     </Layout>
   )
