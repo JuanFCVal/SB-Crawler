ce65d4984f310cd7c26ebd995bed6db815265052
JuanFCVal <cevallosjuanfrancisco@gmail.com>
2024-06-26 22:21:26 -0500

    :sparkles: news card component

---
+++ apps/crawler-front/src/components/NewsCard/NewsCard.tsx
@@ -0,0 +1,22 @@
+import { MessageCircle, ThumbsUp } from 'lucide-react'
+const NewsCard = () => {
+    return (
+        <div className="w-full p-4 bg-slate-200/65 rounded-md">
+            <div className="flex items-center">
+                <h2 className="text-xl font-semibold">1. The Forth Deck mini: a portable Forth computer with a discrete CPU</h2>
+            </div>
+            <div className='flex justify-end text-gray-600 mt-2'>
+                <div className='hover:text-blue-500 hover:font-medium flex cursor-pointer '>
+                    20 comments
+                    <MessageCircle className='mx-2' />
+                </div>
+                <div className='hover:text-blue-500 hover:font-medium flex cursor-pointer '>
+                    15 points
+                    <ThumbsUp className='mx-2' />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NewsCard
