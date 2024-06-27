c11377611e302fc62df4f56c9ad52b49f2c157cf
JuanFCVal <cevallosjuanfrancisco@gmail.com>
2024-06-26 21:47:41 -0500

    :sparkles:  Create layout

---
+++ apps/crawler-front/src/components/navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+
+const Navbar = () => {
+    return (
+        <header>
+            <div className="w-full flex px-8 py-2 items-center border-b-sky-200 border-2">
+                <div className="flex-1">
+                    <img className="transition-transform duration-500 transform hover:scale-105 cursor-pointer" src="https://cdn.stackbuilders.com/media/images/Logo-Website.original.png" alt="logo" />
+                </div>
+                <div >
+                    <span className="font-medium text-md">Juan Francisco Cevallos</span>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Navbar
