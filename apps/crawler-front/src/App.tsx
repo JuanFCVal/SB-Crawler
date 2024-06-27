2bc2cb957589cb8264054d458e6d256ff19c31e2
JuanFCVal <cevallosjuanfrancisco@gmail.com>
2024-06-26 22:37:36 -0500

    :sparkles: set main page structure

--- apps/crawler-front/src/App.tsx
+++ apps/crawler-front/src/App.tsx
@@ -1,11 +1,15 @@
-import './App.css'
+import Layout from "./components/Layout/Layout"
+import NewsList from "./components/NewsList/Newslist"
 
 function App() {
 
   return (
-    <>
-      <h1>Vite + React</h1>
-    </>
+    <Layout>
+      <div className="w-full px-10 md:px-5 sm:px-2">
+        <h1 className="text-3xl mb-4">Hacker news</h1>
+        <NewsList />
+      </div>
+    </Layout>
   )
 }
 
