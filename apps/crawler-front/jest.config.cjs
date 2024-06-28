d1f2b66449c2df4eaab1c0ce0075d7ecb09f913a
JuanFCVal <cevallosjuanfrancisco@gmail.com>
2024-06-28 14:04:56 -0500

    :heavy_plus_sign: Dependencies testing front

---
+++ apps/crawler-front/jest.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1"
+  },
+};
