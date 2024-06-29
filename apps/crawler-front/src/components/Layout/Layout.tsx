5f38efb300c5415a4229a0a5fc061cbfc9a1949a
JuanFCVal <cevallosjuanfrancisco@gmail.com>
2024-06-28 19:50:26 -0500

    :sparkles: add social media to navbar

--- apps/crawler-front/src/components/Layout/Layout.tsx
+++ apps/crawler-front/src/components/Layout/Layout.tsx
@@ -8,8 +8,12 @@ const Layout = ({ children }: ILayoutProps) => {
     return (
         <div className="flex flex-col">
             <Navbar />
-            <div className="w-full mt-24">
+            <div className="w-full mt-24 mb-12">
                 {children}
+            </div>
+            <div className="flex-col items-center justify-center w-full fixed bottom-0 bg-sky-100 py-2 text-gray-600 hover:text-black">
+                <a href="https://github.com/JuanFCVal/SB-Crawler-Monorepo"> <p className="text-center">Made with <span className="animate-pulse">💚</span> and React</p>
+                    <p className="text-center ">Find the code here</p></a>
             </div>
         </div>
     );
