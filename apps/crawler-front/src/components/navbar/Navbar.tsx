9a4412a29329be490a0be48aaa7b40b85144918c
JuanFCVal <cevallosjuanfrancisco@gmail.com>
2024-06-28 19:50:38 -0500

    :sparkles: add social media to navbar

--- apps/crawler-front/src/components/navbar/Navbar.tsx
+++ apps/crawler-front/src/components/navbar/Navbar.tsx
@@ -6,8 +6,14 @@ const Navbar = () => {
                 <div className="flex-1">
                     <img className="transition-transform duration-500 transform hover:scale-105 cursor-pointer" src="https://cdn.stackbuilders.com/media/images/Logo-Website.original.png" alt="logo" />
                 </div>
-                <div >
-                    <span className="font-medium text-md">Juan Francisco Cevallos</span>
+                <div className="flex gap-4 items-center">
+                    <span className="font-medium text-md text-gray-600 md:text-xl sm:text-normal">Juan Francisco Cevallos</span>
+                    <a href="https://www.linkedin.com/in/juanfcval/">
+                        <img className="cursor-pointer hover:scale-110" src="https://iconape.com/wp-content/files/yd/367773/svg/logo-linkedin-logo-icon-png-svg.png" width={30} height={30} alt="Linkedin logo" />
+                    </a>
+                    <a href="https://github.com/JuanFCVal">
+                        <img className="cursor-pointer hover:scale-110" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" width={30} height={30} alt="Github logo" />
+                    </a>
                 </div>
             </div>
         </header>
